f69ba392d47cba54884a2d4d008cb6db5559039a
Hangole <hangole@Hangoleui-iMac.local>
2018-11-12 15:14:34 +0900

    Added very simple contracts

---
+++ VerySimpleContract.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.24;
+// ----------------------- Public Network Part -----------------------
+contract VerySimpleContract {
+    
+    VerySimpleContractCore private privateNetwork;
+    
+    event onSetData (
+        bytes32 data,
+        uint256 timestamp
+    );
+    
+    event setDataCore (
+        bytes32 data
+    );
+    
+    function setPrivateNetwork(address newPrivateNetwork) public {
+        privateNetwork = VerySimpleContractCore(newPrivateNetwork);
+    }
+    
+    function setData(bytes32 data) public {
+        emit setDataCore(data);
+    }
+    
+    function callbackOnSetData(bytes32 data, uint256 timestamp) public {
+        emit onSetData(data, timestamp);
+    }
+    
+}
+// ----------------------- Private Network Part -----------------------
+contract VerySimpleContractCore {
+    
+    VerySimpleContract private publicNetwork;
+    
+    bytes32 private simpleData;
+    
+    event callbackOnSetData (
+        bytes32 data,
+        uint256 timestamp
+    );
+    
+    function setPublicNetwork(address newPublicNetwork) public {
+        publicNetwork = VerySimpleContract(newPublicNetwork);
+    }
+    
+    function setDataCore(bytes32 data) public {
+        simpleData = data;
+        
+        emit callbackOnSetData(simpleData, now);
+    }
+    
+    function getData() public view returns(bytes32) {
+        return (simpleData);
+    }
+    
+}
